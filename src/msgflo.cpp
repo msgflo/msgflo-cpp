f588c6297eb119769f069bbac0e2a6040a037aa3
Jon Nordby <jononor@gmail.com>
2017-05-29 21:11:58 +0200

    discovery: Automatically set .id based on .role

--- src/msgflo.cpp
+++ src/msgflo.cpp
@@ -11,6 +11,22 @@ using namespace trygvis::mqtt_support;
 
 namespace msgflo {
 
+std::string random_string( size_t length )
+{
+    auto randchar = []() -> char
+    {
+        const char charset[] =
+        "0123456789"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
+        const size_t max_index = (sizeof(charset) - 1);
+        return charset[ rand() % max_index ];
+    };
+    std::string str(length,0);
+    std::generate_n( str.begin(), length, randchar );
+    return str;
+}
+
 std::string string_to_upper_copy(const std::string &str) {
     std::string ret;
     ret.resize(str.size());
@@ -145,6 +161,11 @@ protected:
 
     Definition validateDefinitionFromUser(const Definition &definition) {
         Definition d(definition);
+
+        if (d.id.size() == 0) {
+            d.id = d.role + "-" + random_string(8);
+        }
+
         for (auto &port : d.inports) {
             if (port.queue.empty()) {
                 port.queue = generateQueueName(definition, port);
